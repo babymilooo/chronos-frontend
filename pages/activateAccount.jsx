1546e546a1de65fd2adc400b4061725c353bc92d
babymilooo <Kyrylo.Yehorov@cit.khpi.edu.ua>
2024-03-17 13:03:48 +0200

    Fix: redisign activate account page

--- pages/activateAccount.jsx
+++ pages/activateAccount.jsx
@@ -3,7 +3,7 @@ import { RootStoreContext } from '@/app/provider/rootStoreProvider';
 import AuthService from '@/app/services/AuthService';
 import { Button, Image } from '@nextui-org/react';
 import Router from 'next/router';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 
 const ActivateAccount = () => {
     const rootStore = useContext(RootStoreContext);
@@ -40,10 +40,14 @@ const ActivateAccount = () => {
             <p className="text-4xl font-bold">Activate Account</p>
             <div className="flex flex-col items-center">
 
-                <p className="text-center mt-5 mb-5">Check your email and enter the code we just sent to you</p>
+                <p className="text-center mt-4 mb-5">Check your email and enter the code we just sent to you</p>
             </div>
             <form className="flex flex-col items-center justify-center" onSubmit={handleSubmit}>
                 <InputOTPPattern onChange={(value) => setPassword(value)} />
+                <div className="flex items-center justify-center mt-4">
+                    <p>Didn't receive the code?</p>
+                    <p className="text-content hover:underline cursor-pointer ml-1 font-bold">Resend</p>
+                </div>
                 <Button color="default" variant="bordered" type='submit' className="mt-4 hover:border-content">
                     Activate
                 </Button>
