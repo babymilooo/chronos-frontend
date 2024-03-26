2306a15cd8bfb60af630041e4e4b4fb8a9a64eb1
babymilooo <Kyrylo.Yehorov@cit.khpi.edu.ua>
2024-03-26 15:46:18 +0200

    Fix: Fix popover in navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,6 +9,7 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { RootStoreContext } from '@/app/provider/rootStoreProvider';
 import Link from 'next/link';
 import { Button } from './ui/button';
+import Router from 'next/router';
 // import { useRouter } from 'next/router';
 // import UserService from '@/app/services/UserService';
 
@@ -60,41 +61,42 @@ const Navbar = () => {
                         <MyPopoverTrigger asChild>
                             <Avatar className="cursor-pointer">
                                 <AvatarImage src={userStore.user.image} alt="User avatar" />
-                                <AvatarFallback>CN</AvatarFallback> 
+                                <AvatarFallback>CN</AvatarFallback>
                             </Avatar>
                         </MyPopoverTrigger>
                         <MyPopoverContent className="rounded-3xl border border-foreground2 shadow-lg" align="start">
-                            <div className="bg-background2">
-                                <div className="flex items-center rounded-t-3xl p-4">
-                                    <Avatar>
-                                        <AvatarImage src={userStore.user.image} alt="User avatar" />
-                                        <AvatarFallback>CN</AvatarFallback>
-                                    </Avatar>
-                                    <Link href={`/users/${userStore.user.id}`} className="ml-2">
-                                        <p className="font-bold text-lg">{userStore.user.username}</p>
-                                        <p className="text-neutral-500">{userStore.user.email}</p>
-                                    </Link>
+                            <Link href={`/users/${userStore.user.id}`} className="flex items-center bg-background2 rounded-t-3xl border-b border-foreground2 p-4">
+                                <Avatar>
+                                    <AvatarImage src={userStore.user.image} />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                                <div>
+                                    <p className="font-bold text-lg">{userStore.user.username}</p>
+                                    <p className="text-neutral-500">{userStore.user.email}</p>
                                 </div>
-                                <div className="p-4">
-                                    <Link href={`/users/${userStore.user.id}/friends`} className="block mb-2 text-blue-600 hover:bg-blue-50 hover:text-blue-600 p-2 rounded-md">
-                                        Friends
-                                    </Link>
-                                    <Link href={`/users/${userStore.user.id}/settings`} className="block mb-2 text-blue-600 hover:bg-blue-50 hover:text-blue-600 p-2 rounded-md">
-                                        Settings
-                                    </Link>
-                                    <Button
-                                        className="w-full text-red-600 hover:bg-red-50 hover:text-red-600 p-2 rounded-md"
-                                        onClick={handleLogout}
-                                    >
-                                        Log Out
-                                    </Button>
-                                </div>
+                            </Link>
+
+                            <div className="p-4">
+                                <Link href={`/users/${userStore.user.id}/friends`} className="block mb-2 hover:bg-background2 p-2 rounded-md">
+                                    Friends
+                                </Link>
+                                <Link href={`/users/${userStore.user.id}/settings`} className="block mb-2 hover:bg-background2 p-2 rounded-md">
+                                    Settings
+                                </Link>
+                                <Button
+                                    className="w-full p-2 rounded-md hover:bg-red-500 hover:text-foreground2"
+                                    onClick={handleLogout}
+                                    variant="ghost"
+                                >
+                                    Log Out
+                                </Button>
                             </div>
+
                         </MyPopoverContent>
                     </MyPopover>
                 </div>
             </div>
-        </nav>
+        </nav >
     );
 };
 
